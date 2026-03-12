715e49b455a6c6e608355e1d45198d561d1e9920
sixzeras <odaniil1337@gmail.com>
2026-03-12 17:36:05 +0300

    Update main.cpp

--- task_one/main.cpp
+++ task_one/main.cpp
@@ -1,11 +1,16 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include <backend.h>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
+    Backend backend;
+
     QQmlApplicationEngine engine;
+    engine.rootContext()->setContextProperty("backend", &backend);
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
